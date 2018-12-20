3b0f9728dc8ca19fbb940d58590a110b1779868e
Rick Waldron <waldron.rick@gmail.com>
2018-12-19 19:16:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+npx rwaldron
+npx rickwaldron
+```
+
+![](https://i.gyazo.com/1dce24c5cbb06b47cce1018d0e01228b.png)
---
+++ index.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+const boxen = require("boxen");
+const {stripIndent} = require("common-tags");
+const csi = "\x1b";
+const {grey, magenta, bold} = [
+  ["bold", 1, 22],
+  ["magenta", 35, 39],
+  ["grey", 90, 39 ],
+].reduce((accum, [name, prefix, suffix]) =>
+  // nod to @darrenjennings
+  (accum[name] = input => `${csi}[${prefix}m${input}${csi}[${suffix}m`, accum), {}
+);
+console.log(
+  boxen(
+  stripIndent`
+    ${bold(magenta("Rick Waldron"))}
+    ${bold(grey("Director of Engineering @ Bocoup"))}
+
+    ${grey("https://github.com/rwaldron")}
+    ${grey("https://twitter.com/rwaldron")}
+  `,
+  {
+    borderColor: "magenta",
+    borderStyle: "round",
+    dimBorder: true,
+    margin: 1,
+    padding: 1
+  })
+);
